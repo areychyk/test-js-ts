4bbffc2acf2f83ff8c32edfdcf0f55d304930bb0
Ariiychyk Denis <areychyk@mail.ru>
2023-01-11 20:19:58 +0300

    Les05

---
+++ src/Test-09_mutability/user.test.ts
@@ -0,0 +1,74 @@
+function icreaseAge(u: UserType) {
+    u.age++
+}
+
+type UserType = {
+    name: string
+    age: number
+}
+
+test('reference type test', () => {
+    let user: UserType = {
+        name: 'Denis',
+        age: 25
+    }
+
+    icreaseAge(user)
+
+    expect(user.age).toBe(26)
+
+    const newUser = user
+    newUser.age = 1000
+    expect(user.age).toBe(1000)
+    icreaseAge(user)
+    expect(user.age).toBe(1001)
+
+})
+
+test('array reference test', () => {
+    let users = [
+        {
+            name: 'Denis',
+            age: 25
+        },
+        {
+            name: 'Denis',
+            age: 25
+        },
+    ]
+
+    let admins = users
+
+    admins.push({
+        name: 'Array',
+        age: 5
+    })
+
+
+    expect(users[2]).toEqual({
+        name: 'Array',
+        age: 5
+    })
+
+})
+
+
+test('value type test', () => {
+    let userCount = 100;
+    let adminsCount = userCount
+
+
+
+
+})
+
+
+test('sort array test', () => {
+    let letters = ['c','z','d']
+    letters.sort()
+
+
+    expect(letters).toEqual(['c','d','z'])
+
+
+})
---
+++ src/Test-09_mutability/user.ts
@@ -0,0 +1,10 @@
+
+let user={
+    name: 'Denis',
+    age:25
+}
+
+
+function icreaseAge(user:any){
+    user.age++
+}
